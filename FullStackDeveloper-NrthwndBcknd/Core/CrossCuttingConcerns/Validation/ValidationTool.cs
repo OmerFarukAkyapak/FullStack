41e2037f37ee735ed64b61d2e3681fffe81a97ee
OmerFarukAkyapak <56655714+OmerFarukAkyapak@users.noreply.github.com>
2023-09-03 15:03:13 +0300

    .

--- FullStackDeveloper-NrthwndBcknd/Core/CrossCuttingConcerns/Validation/ValidationTool.cs
+++ FullStackDeveloper-NrthwndBcknd/Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -7,9 +7,14 @@ namespace Core.CrossCuttingConcerns.Validation
 {
     public static class ValidationTool
     {
-        public static void Validate(IValidator validator)
+        public static void Validate(IValidator validator, object entity)
         {
             
+            var result = validator.Validate(entity);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
         }
     }
 }
